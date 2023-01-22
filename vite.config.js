4480e423c9f8af5404e98ae569ca22d6c7432a14
Phil_nguyxn <khoinm2003@gmail.com>
2023-01-22 18:23:22 +0700

    Remove Vitest, RTL, Jest-DOM
    Remove them since we'll use Cypress

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.js',
-  },
 })
