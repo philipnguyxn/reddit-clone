1d01601ecd2595839f3ee3efc94d5a82a44516dc
Phil_nguyxn <khoinm2003@gmail.com>
2023-01-22 18:22:28 +0700

    Add default themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'bg-primary': '#d9d9d9',
+      'bg-secondary': '#fff',
+      'bg-field': '#f6f7f8',
+      'bg-likes': '#edeff1',
+      'button-primary': '#ff4500',
+    },
+    fontFamily: {
+      sans: ['Roboto', "sans-serif"]
+    },
+    extend: {
+      borderRadius: {
+        's': '0.0625rem'
+      }
+    },
   },
   plugins: [],
 }
