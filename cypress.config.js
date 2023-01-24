6fc85e385dfba37ddeb4de6ddc46a249b747a5aa
Phil_nguyxn <khoinm2003@gmail.com>
2023-01-24 21:28:12 +0700

    Init Cypress' component testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
+// Node Modules
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: '59ns47',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+      },
+    },
+  },
+);
