343ee70a859616ab292ce02a6e2eb7bfc27b0a17
Phil_nguyxn <khoinm2003@gmail.com>
2023-01-25 08:20:43 +0700

    Integrate Tailwind CSS with MUI (cont)

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 // router
 import router from "./router";
@@ -16,9 +17,11 @@ import store from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssBaseline />
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
+    <StyledEngineProvider>
+      <CssBaseline />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
