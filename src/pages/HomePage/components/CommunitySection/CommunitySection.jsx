6b4dd9163f3daef9ce2497cde82d3257edcc3418
Phil_nguyxn <khoinm2003@gmail.com>
2023-02-06 10:32:13 +0700

    CommunitySection UI bug fix

--- src/pages/HomePage/components/CommunitySection/CommunitySection.jsx
+++ src/pages/HomePage/components/CommunitySection/CommunitySection.jsx
@@ -9,32 +9,32 @@ import { ReactComponent as User } from "../../../../assets/icons/user.svg";
 
 const CommunitySection = () => {
   return (
-    <div className="col-span-1 row-span-2 block bg-secondary rounded">
+    <>
       <div className="mt-4">
         <span className="text-center">
           <h2>Communities</h2>
         </span>
       </div>
       {/* Community List */}
-      <ul className="mt-6 px-5 grid grid-cols-1 grid-rows-1 gap-3 list-none">
+      <ul className="mt-6 px-5 list-none">
         {communities.map((community) => (
-          <li key={community.id}>
-            <div className="flex items-center">
+          <>
+            <li key={community.id} className="w-full flex items-center mb-3">
               <span className="mr-1.5">
                 <User />
               </span>
               <span>
                 <h4>{community.name}</h4>
               </span>
-            </div>
+            </li>
             <div
               className="w-full border-divider border-solid 
-                      border-primary last:border-none"
+                  border-primary last:border-none mb-3"
             ></div>
-          </li>
+          </>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
