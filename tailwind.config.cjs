7898f90ed06ad5c87ba41773c146ccf021e5b410
Phil_nguyxn <khoinm2003@gmail.com>
2023-02-06 10:14:48 +0700

    HomePage UI bug fix
    Change in the display of sections in the home page. Grid with many rows and cols, now only with 2 cols

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -48,7 +48,7 @@ module.exports = {
       },
       padding: {
         'switch': '0.1875rem',
-        'primary--x': '4.5rem',
+        'primary--x': '18rem',
         'primary--t': '1.25rem',
       },
       spacing: {
